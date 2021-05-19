f896799602b05459a34cddc2123278ef93573dbc
Ankit Sachan <ankit@rewardz.sg>
2021-05-19 08:45:53 +0530

    public init method

--- PodTrial/SwiftyLib.swift
+++ PodTrial/SwiftyLib.swift
@@ -10,7 +10,7 @@ import Foundation
 public final class SwiftyLib {
 
     let name = "SwiftyLib"
-    
+    public init(){}
     public func add(a: Int, b: Int) -> Int {
         return a + b
     }
