568907772f76c1ec097db09f8a75d1d0b02b88b2
Ankit Sachan <ankit@rewardz.sg>
2021-05-19 07:30:49 +0530

    Initial project setup

---
+++ PodTrial/PodTrial.h
@@ -0,0 +1,18 @@
+//
+//  PodTrial.h
+//  PodTrial
+//
+//  Created by Ankit Sachan on 19/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PodTrial.
+FOUNDATION_EXPORT double PodTrialVersionNumber;
+
+//! Project version string for PodTrial.
+FOUNDATION_EXPORT const unsigned char PodTrialVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodTrial/PublicHeader.h>
+
+
