480cfe1582a798f8802f02f44ed54cf6681265e6
Ankit Sachan <ankit@rewardz.sg>
2021-05-19 09:38:38 +0530

    updated name

--- PodTrial/SwiftyLib.swift
+++ PodTrial/SwiftyLib.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public final class SwiftyLib {
 
-    public let name = "SwiftyLib"
+    public let name = "SwiftyLib-name updated"
     public init(){}
     public func add(a: Int, b: Int) -> Int {
         return a + b
